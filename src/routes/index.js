f03a9b8c1e26e796728d5ba6de6b94a217cd088a
YoungJinnn <yjbulee@postech.ac.kr>
2023-01-27 21:38:31 +0900

    cookies modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { Main, MyPage } from "./pages";
 import NotFound from "./NotFound";
+import { useEffect } from "react";
 
 const MainRoutes = () => {
+  const [cookies] = useCookies(["loginkey"]);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(cookies.loginkey === undefined ){
+        navigate("/login");
+      } else if ( cookies.loginkey ){
+        navigate("/main");
+      } else {
+        console.log("correct routes");
+      }
+  }, [cookies.sessionKey, location.pathname]);
+
+
   return (
     <Routes>
       <Route path="/" element={<Main />}></Route>
