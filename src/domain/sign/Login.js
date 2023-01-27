8533510dc57fb4638190e7a6cf62f95220929cd0
YoungJinnn <yjbulee@postech.ac.kr>
2023-01-28 00:10:36 +0900

    sign

--- src/domain/sign/Login.js
+++ src/domain/sign/Login.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import { useSnackbar } from 'notistack';
+import { Loginform } from '../../components/sign';
 
 const Login = () => {
+  const { enqueueSnackbar } = useSnackbar();
+  const [cookies, setCookie,] = useCookies(['loginkey']);
+  const navigate = useNavigate();
+  const [login, setLogin] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChangeLogin = (event) => {
+    let changeLogin = { ...login };
+    changeLogin[event.target.name] = event.target.value;
+    setLogin(changeLogin);
+  };
+
+  const handleClickLogin = async () => {
+    await axios ({
+        method: 'post',
+        url: `/api/login/${cookies.loginkey}`,
+        data: {
+            email: login.email,
+            password: login.password,
+        }
+    })
+    .then((response)=>{
+        console.log(response);
+        if (res.data.login === false) {
+            enqueueSnackbar(`아이디/비밀번호를 다시 확인해주세요`, { variant: 'error' });
+        }
+        else if (res.data.login === true) {
+            enqueueSnackbar('Surround에 오신 것을 환영합니다', { variant: 'info' });
+            setCookie("loginkey", res.data.Id, { path: '/' });
+            setCookie("name", res.data.Name, { path: '/' });              //
+            navigate('/')
+        }
+    }).catch((Error)=>{
+        console.log(Error);
+    })
+  }
+
   return (
-    <div>Login</div>
+    <Loginform
+        login={login}
+        handleChangeLogin={handleChangeLogin}
+        handleClickLogin={handleClickLogin}
+    />
   )
 };
 
--- src/domain/sign/SignUp.js
+++ src/domain/sign/SignUp.js
@@ -15,20 +15,37 @@ const Signup = () => {
     passwordCheck: '',
   });
 
+  const handleChangeSignup = (event) => {
+    let changeSignup = { ...signup };
+    changeSignup[event.target.name] = event.target.value;
+    setSignup(changeSignup);
+  };
+
   const handleClickSignUp = async () => {
     await axios ({
         method: 'post',
-        url: 'api/selfintro',
+        url: `/api/signup/${cookies.loginkey}`,
         data: {
             email: signup.email,
             nickname: signup.nickname,
             password: signup.password,
         }
     })
+    .then((response)=>{
+        console.log(response);
+        setCookie("loginkey", res.data.Id, { path: '/' });   //res.data.Id
+        navigate('/');
+    }).catch((Error)=>{
+        console.log(Error);
+    })
   }
 
   return (
-    <Signupform/>
+    <Signupform
+        signup={signup}
+        handleChangeSignup={handleChangeSignup}
+        handleClickSignUp={handleClickSignUp}
+    />
   )
 };
 
