82676fd2282630725772f86c557b57c1147ad28c
YoungJinnn <yjbulee@postech.ac.kr>
2023-01-28 02:24:40 +0900

    signup_design

--- src/components/sign/Signupform.js
+++ src/components/sign/Signupform.js
@@ -37,67 +37,81 @@ const Signupform = (props) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container justifyContent='center'>
-        <Grid xs={12} sm={9} md={6} lg={4.8} xl={3.5} className='signup_div'>
-          {/* <img src={logo} alt='logo' /> */}
-          <h1>회원가입</h1>
+      <Grid container className='sign_background' justifyContent='center' alignItems='center'>
+        <Grid item container className='sign_container' xs={12} sm={10} md={9} lg={8} xl={8}>
+            <Grid item xs={12} sm={6} md={5} lg={4} xl={4}>
+                <Grid className='signup_div'>
+                    {/* <img src={logo} alt='logo' /> */}
+                    <h1>회원가입</h1>
 
-          <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
-            <InputLabel shrink>이메일</InputLabel>
-            <OutlinedInput className='signup_input'
-              sx={{ height: '48px' }}
-              color='warning'
-              label='이메일'
-              type='email'
-              name='email'
-              placeholder='이메일을 입력해 주세요'
-              value={props.signup.email}
-              onChange={props.handleChangeSignup}
-            />
-          </FormControl>
-          <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
-            <InputLabel shrink>비밀번호</InputLabel>
-            <OutlinedInput className='signup_input'
-              sx={{ height: '48px' }}
-              color='warning'
-              label='비밀번호'
-              type='password'
-              name='password'
-              placeholder='비밀번호를 입력해 주세요'
-              value={props.signup.password}
-              onChange={props.handleChangeSignup}
-            />
-          </FormControl>
-          <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
-            <InputLabel shrink>비밀번호 확인</InputLabel>
-            <OutlinedInput className='signup_input'
-              sx={{ height: '48px' }}
-              color='warning'
-              label='비밀번호 확인'
-              type='password'
-              name='passwordCheck'
-              placeholder='비밀번호를 한번 더 입력해 주세요'
-              value={props.signup.passwordCheck}
-              onChange={props.handleChangeSignup}
-            />
-          </FormControl>
+                    <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
+                        <InputLabel shrink>이메일</InputLabel>
+                        <OutlinedInput className='signup_input'
+                        sx={{ height: '48px' }}
+                        color='warning'
+                        label='이메일'
+                        type='email'
+                        name='email'
+                        placeholder='이메일을 입력해 주세요'
+                        value={props.signup.email}
+                        onChange={props.handleChangeSignup}
+                        />
+                    </FormControl>
+                    <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
+                        <InputLabel shrink>비밀번호</InputLabel>
+                        <OutlinedInput className='signup_input'
+                        sx={{ height: '48px' }}
+                        color='warning'
+                        label='비밀번호'
+                        type='password'
+                        name='password'
+                        placeholder='비밀번호를 입력해 주세요'
+                        value={props.signup.password}
+                        onChange={props.handleChangeSignup}
+                        />
+                    </FormControl>
+                    <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
+                        <InputLabel shrink>비밀번호 확인</InputLabel>
+                        <OutlinedInput className='signup_input'
+                        sx={{ height: '48px' }}
+                        color='warning'
+                        label='비밀번호 확인'
+                        type='password'
+                        name='passwordCheck'
+                        placeholder='비밀번호를 한번 더 입력해 주세요'
+                        value={props.signup.passwordCheck}
+                        onChange={props.handleChangeSignup}
+                        />
+                    </FormControl>
 
-          <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
-            <InputLabel shrink>닉네임</InputLabel>
-            <OutlinedInput className='signup_input'
-              sx={{ height: '48px' }}
-              color='warning'
-              label='닉네임'
-              type='text'
-              name='nickname'
-              placeholder='닉네임을 입력해 주세요'
-              value={props.signup.major}
-              onChange={props.handleChangeSignup}
-            />
-          </FormControl>
+                    <FormControl sx={{ width: '75%', marginRight: '0%', marginBottom: '16px' }}>
+                        <InputLabel shrink>닉네임</InputLabel>
+                        <OutlinedInput className='signup_input'
+                        sx={{ height: '48px' }}
+                        color='warning'
+                        label='닉네임'
+                        type='text'
+                        name='nickname'
+                        placeholder='닉네임을 입력해 주세요'
+                        value={props.signup.major}
+                        onChange={props.handleChangeSignup}
+                        />
+                    </FormControl>
 
-          <Button color='warning' variant="outlined" disabled={props.state} onClick={props.handleClickSignUp}>회원가입</Button>
+                    <Button color='warning' variant="outlined" disabled={props.state} onClick={props.handleClickSignUp}>회원가입</Button>
+                    </Grid>
+            </Grid>
+            <Grid item xs={12} sm={6} md={7} lg={8} xl={8} justifyContent='center' alignItems='center'>
+                <Grid className='sign_info'>
+                    <div>Upload and share your music</div>
+                    <div>Follow your favorite artists</div>
+                    <div>Create and publish playlists</div>
+                    <div>And much more</div>
+                </Grid>
+            </Grid>
         </Grid>
+
+        
       </Grid>
     </ThemeProvider>
   )
