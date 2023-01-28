f0d2da12e23188a8b8975df280ccad3a9a7b8959
조영록 <54888076+0-ROK@users.noreply.github.com>
2023-01-28 13:13:42 +0900

    카카오 로그인 토큰 확보까지 완료

--- src/domain/sign/SignUp.js
+++ src/domain/sign/SignUp.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
 import axios from "axios";
-import { Signupform } from 'components/sign';
+import { Signupform } from "components/sign";
 
 const Signup = () => {
-  const [cookies, setCookie,] = useCookies(['loginkey']);
+  const [cookies, setCookie] = useCookies(["loginkey"]);
   const navigate = useNavigate();
 
   const [signup, setSignup] = useState({
-    email: '',
-    nickname: '',
-    password: '',
-    passwordCheck: '',
+    email: "",
+    nickname: "",
+    password: "",
+    passwordCheck: "",
   });
 
   const handleChangeSignup = (event) => {
@@ -22,31 +22,32 @@ const Signup = () => {
   };
 
   const handleClickSignUp = async () => {
-    await axios ({
-        method: 'post',
-        url: `/api/signup/${cookies.loginkey}`,
-        data: {
-            email: signup.email,
-            nickname: signup.nickname,
-            password: signup.password,
-        }
+    await axios({
+      method: "post",
+      url: `/api/signup/${cookies.loginkey}`,
+      data: {
+        email: signup.email,
+        nickname: signup.nickname,
+        password: signup.password,
+      },
     })
-    .then((res)=>{
+      .then((res) => {
         console.log(res);
-        setCookie("loginkey", res.data.Id, { path: '/' });   //res.data.Id
-        navigate('/');
-    }).catch((Error)=>{
+        setCookie("loginkey", res.data.Id, { path: "/" }); //res.data.Id
+        navigate("/");
+      })
+      .catch((Error) => {
         console.log(Error);
-    })
-  }
+      });
+  };
 
   return (
     <Signupform
-        signup={signup}
-        handleChangeSignup={handleChangeSignup}
-        handleClickSignUp={handleClickSignUp}
+      signup={signup}
+      handleChangeSignup={handleChangeSignup}
+      handleClickSignUp={handleClickSignUp}
     />
-  )
+  );
 };
 
 export default Signup;
