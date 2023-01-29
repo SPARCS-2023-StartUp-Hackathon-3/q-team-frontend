94e49551b702dc7b006206b8bf25111a473998fb
YoungJinnn <yjbulee@postech.ac.kr>
2023-01-29 09:20:09 +0900

    feedform

--- src/components/common/WritePost.js
+++ src/components/common/WritePost.js
@@ -2,15 +2,19 @@ import { Button, InputBase } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import icon_musicalnotes from "img/icon_musicalnotes.png";
 
-const WritePost = () => {
+const WritePost = (props) => {
   return (
-    <Box>
+    <Box sx={{ Width: "100%", backgroundColor: "#F8F8F8", boxShadow: "none", padding: "20px"}}>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <InputBase
-          sx={{ ml: 1, flex: 1 }}
+          className="writepost"
           placeholder="What's happening?"
-          inputProps={{ "aria-label": "search google maps" }}
+          sx={{marginBottom: "50px"}}
+          name='contents'
+          value={props.writePost.contents}
+          onChange={props.handleChangeWritePost}
         />
         <div
           style={{
@@ -21,14 +25,18 @@ const WritePost = () => {
         >
           <Button
             variant="outlined"
-            color="primary"
-            sx={{ p: "10px" }}
-            aria-label="directions"
+            color="White" 
+            sx={{ height: "50px", width: "50px", padding: "0px", minWidth: "0px", boxShadow: "0px 0px 7px rgba(0, 0, 0, 0.19)", borderRadius: "8px"}}
           >
-            <MusicNoteIcon />
+            <img src={icon_musicalnotes} alt="icon_musicalnotes" style={{height: "50%"}} />
           </Button>
 
-          <Button variant="contained" style={{ background: "#999999" }}>
+          <Button 
+            variant="contained" color="Primary1" sx={{color: "#ffffff"}}
+            disabled={
+              props.writePost.contents && props.writePost.musicfiles ? false : true
+            }
+          >
             Post
           </Button>
         </div>
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,5 +1,3 @@
-export { default as Mainform } from './Mainform';
-
 export { default as Feedform } from './Feedform';
 export { default as PrivateFeedform } from './PrivateFeedform';
 export { default as Searchform } from './Searchform';
